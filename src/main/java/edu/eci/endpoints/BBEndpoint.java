3685661d8936991721022f904992edfd7b35f23d
juanzambranog <142639568+juanzambranog@users.noreply.github.com>
2025-10-31 00:14:20 -0500

    Avance workspace

---
+++ src/main/java/edu/eci/endpoints/BBEndpoint.java
@@ -0,0 +1,67 @@
+package edu.eci.endpoints;
+
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+@ServerEndpoint("/bbService")
+public class BBEndpoint {
+
+    private static final Logger logger = Logger.getLogger(BBEndpoint.class.getName());
+    static Queue<Session> queue = new ConcurrentLinkedQueue<>();
+    Session ownSession = null;
+
+    public void send(String msg) {
+        try {
+            for (Session session : queue) {
+                if (!session.equals(this.ownSession)) {
+                    session.getBasicRemote().sendText(msg);
+                }
+                logger.log(Level.INFO, "Sent: {0}", msg);
+            }
+        } catch (IOException e) {
+            logger.log(Level.INFO, e.toString());
+        }
+    }
+
+    @OnMessage
+    public void processPoint(String message, Session session) {
+        System.out.println("Point received:" + message + ". From session: " + session);
+        this.send(message);
+    }
+
+    @OnOpen
+    public void openConnection(Session session) {
+        queue.add(session);
+        ownSession = session;
+        logger.log(Level.INFO, "Connection opened.");
+        try {
+            session.getBasicRemote().sendText("Connection established.");
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @OnClose
+    public void closedConnection(Session session) {
+        queue.remove(session);
+        logger.log(Level.INFO, "Connection closed.");
+    }
+
+    @OnError
+    public void error(Session session, Throwable t) {
+        queue.remove(session);
+        logger.log(Level.INFO, t.toString());
+        logger.log(Level.INFO, "Connection error.");
+    }
+}
--- src/main/resources/static/js/bbComponents.jsx
+++ src/main/resources/static/js/bbComponents.jsx
@@ -1,4 +1,95 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-<h1>Bienvenido</h1>
-);
+
+function BBServiceURL() {
+  var host = window.location.host;
+  var url = 'wss://' + (host) + '/bbService';
+  if (host.toString().startsWith("localhost")) {
+    url = 'ws://' + (host) + '/bbService';
+  }
+  return url;
+}
+
+class WSBBChannel {
+  constructor(URL, callback) {
+    this.URL = URL;
+    this.wsocket = new WebSocket(URL);
+    this.wsocket.onopen = (evt) => this.onOpen(evt);
+    this.wsocket.onmessage = (evt) => this.onMessage(evt);
+    this.wsocket.onerror = (evt) => this.onError(evt);
+    this.receivef = callback;
+  }
+
+  onOpen(evt) { console.log("WS open", evt); }
+  onMessage(evt) {
+    if (evt.data != "Connection established.") {
+      this.receivef(evt.data);
+    }
+  }
+  onError(evt) { console.error("WS error", evt); }
+  send(x, y) {
+    let msg = '{ "x": ' + (x) + ', "y": ' + (y) + "}";
+    this.wsocket.send(msg);
+  }
+  close() { this.wsocket.close(); }
+}
+
+function BBCanvas() {
+  const [svrStatus, setSvrStatus] = React.useState({loadingState: 'Loading Canvas...'});
+  const comunicationWS = React.useRef(null);
+  const myp5 = React.useRef(null);
+
+  const sketch = function (p) {
+    p.setup = function () {
+      p.createCanvas(700, 410);
+    }
+
+    p.draw = function () {
+      if (p.mouseIsPressed === true) {
+        p.fill(0, 0, 0);
+        p.ellipse(p.mouseX, p.mouseY, 20, 20);
+        if (comunicationWS.current) comunicationWS.current.send(p.mouseX, p.mouseY);
+      } else {
+        p.fill(255, 255, 255);
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    myp5.current = new p5(sketch, 'container');
+    setSvrStatus({loadingState: 'Canvas Loaded'});
+    comunicationWS.current = new WSBBChannel(BBServiceURL(), (msg) => {
+      var obj = JSON.parse(msg);
+      drawPoint(obj.x, obj.y);
+    });
+    return () => {
+      if (comunicationWS.current) comunicationWS.current.close();
+    };
+  }, []);
+
+  function drawPoint(x, y) {
+    myp5.current.ellipse(x, y, 20, 20);
+  }
+
+  return (
+    <div>
+      <h4>Drawing status: {svrStatus.loadingState}</h4>
+      <div id="container"></div>
+    </div>
+  );
+}
+
+function Editor({name}) {
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+      <hr/>
+      <div id="toolstatus"></div>
+      <hr/>
+      <div id="container"><BBCanvas /></div>
+      <hr/>
+      <div id="info"></div>
+    </div>
+  );
+}
+
+root.render(<Editor name="Daniel" />);
